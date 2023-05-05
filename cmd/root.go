c760136a9cea5e8bb42724d85f8264acfcbfd369
angelina-tsuboi <angelina.t1832@gmail.com>
2023-05-05 09:12:07 -0700

    feat: init osint go

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "SatIntel",
+	Use:   "satintel",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
