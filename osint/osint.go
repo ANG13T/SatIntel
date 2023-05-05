6e4ae80c06536575eda1465725c252f778001485
angelina-tsuboi <angelina.t1832@gmail.com>
2023-05-05 12:42:30 -0700

    feat: display options

--- osint/osint.go
+++ osint/osint.go
@@ -8,7 +8,9 @@ import (
 func Banner() {
 	banner, _ := ioutil.ReadFile("txt/banner.txt")
 	info, _ := ioutil.ReadFile("txt/info.txt")
+	options, _ := ioutil.ReadFile("txt/options.txt")
 	g,_:=gradient.NewGradient("cyan", "blue")
 	g.Print(string(banner))
 	g.Print(string(info))
+	g.Print("\n" + string(options))
 }
