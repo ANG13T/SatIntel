d1677e0ab545fd32b2702201d8d6336dddeeeaf1
angelina-tsuboi <angelina.t1832@gmail.com>
2023-05-05 12:49:09 -0700

    feat: style banner art

--- osint/osint.go
+++ osint/osint.go
@@ -10,7 +10,9 @@ func Banner() {
 	info, _ := ioutil.ReadFile("txt/info.txt")
 	options, _ := ioutil.ReadFile("txt/options.txt")
 	g,_:=gradient.NewGradient("cyan", "blue")
+	solid,_:=gradient.NewGradient("blue", "#1179ef")
+	opt,_:=gradient.NewGradient("#1179ef", "cyan")
 	g.Print(string(banner))
-	g.Print(string(info))
-	g.Print("\n" + string(options))
+	solid.Print(string(info))
+	opt.Print("\n" + string(options))
 }
