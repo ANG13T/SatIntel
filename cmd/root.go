b7e07accccd1ef9655d525265a42923433fb86db
angelina-tsuboi <angelina.t1832@gmail.com>
2023-05-05 10:11:46 -0700

    feat: init `Banner` func

--- cmd/root.go
+++ cmd/root.go
@@ -8,11 +8,9 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/ANG13T/SatIntel/osint"
 )
 
-
-
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "satintel",
 	Short: "A brief description of your application",
@@ -22,13 +20,11 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		osint.Banner()
+	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -37,14 +33,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.SatIntel.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
--- osint/osint.go
+++ osint/osint.go
@@ -2,8 +2,13 @@ package osint
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 )
 
 func Banner() {
-	
+	banner, err := ioutil.ReadFile("../txt/banner.txt")
+	res(err)
+	fmt.Println("\033[37m", string(banner))
 }
