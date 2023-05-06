16d72f7b4e2a1ebe91a435793ef91b70ce2d456a
angelina-tsuboi <angelina.t1832@gmail.com>
2023-05-05 18:11:31 -0700

    feat: init tle_parser options

--- osint/osint.go
+++ osint/osint.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"github.com/TwiN/go-color"
 )
 
 type struct TLE {
@@ -30,10 +31,32 @@ type struct TLE {
 
 func TLEParser(){
 	fmt.Println("Parsing TLE")
+
 }
 
 func TLETextFile(){
-
+	
+	fmt.Print("\n ENTER INPUT > ")
+	var path string
+	file, err := os.Open(path)
+ 
+	if err != nil {
+		fmt.Println(color.Ize(color.Red, "  [!] INVALID TEXT FILE"))
+	}
+ 
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []string
+ 
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+ 
+	file.Close()
+ 
+	for _, eachline := range txtlines {
+		fmt.Println(eachline)
+	}
 }
 
 func TLEPlainString(string input){
