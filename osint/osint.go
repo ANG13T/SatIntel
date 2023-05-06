b3023c440eea34b3588c2cdffc3dc1de653fe63f
angelina-tsuboi <angelina.t1832@gmail.com>
2023-05-05 18:07:19 -0700

    feat: init tle struct

--- osint/osint.go
+++ osint/osint.go
@@ -2,8 +2,40 @@ package osint
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
+
+type struct TLE {
+	CommonName string
+	SatelliteCatalogNumber int
+	ElsetClassificiation string
+	InternationalDesignator string
+	ElementSetEpoch float32
+	FirstDerivativeMeanMotion float32
+	SecondDerivativeMeanMotion string
+	BDragTerm string
+	ElementSetType int
+	ElementNumber int
+	ChecksumOne int
+	OrbitInclination float32
+	RightAscension float32
+	Eccentrcity float32
+	Perigee float32
+	MeanAnamoly float32
+	MeanMotion float32
+	RevolutionNumber int
+	ChecksumTwo int
+}
 
 func TLEParser(){
 	fmt.Println("Parsing TLE")
+}
+
+func TLETextFile(){
+
+}
+
+func TLEPlainString(string input){
+
 }
